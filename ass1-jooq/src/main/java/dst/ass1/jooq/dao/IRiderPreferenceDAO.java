d6be3e710f5aeb4487bbf02076feadf9b2351e40
Philipp <p.raith@dsg.tuwien.ac.at>
2023-03-20 21:33:10 +0100

    Fix database URL and update code docs

--- ass1-jooq/src/main/java/dst/ass1/jooq/dao/IRiderPreferenceDAO.java
+++ ass1-jooq/src/main/java/dst/ass1/jooq/dao/IRiderPreferenceDAO.java
@@ -7,6 +7,7 @@ public interface IRiderPreferenceDAO extends GenericDAO<IRiderPreference> {
   /**
    * Update the preference map of a rider. Adds new preferences or alters existing ones. Does not
    * remove or alter not specified preferences.
+   * Also updates all instance fields of the model.
    * @param model the model containing the preferences to alter or to add
    */
   void updatePreferences(IRiderPreference model);

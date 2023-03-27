fd2cf85e36e1ee684c6e6d592afce6263b6c506e
flofriday <flohacksfriday@gmail.com>
2023-03-27 19:38:41 +0200

    Implement findById for 1.3.4

--- ass1-jooq/src/main/java/dst/ass1/jooq/dao/impl/RiderPreferenceDAO.java
+++ ass1-jooq/src/main/java/dst/ass1/jooq/dao/impl/RiderPreferenceDAO.java
@@ -3,11 +3,14 @@ package dst.ass1.jooq.dao.impl;
 import dst.ass1.jooq.connection.DataSource;
 import dst.ass1.jooq.dao.IRiderPreferenceDAO;
 import dst.ass1.jooq.model.IRiderPreference;
+import dst.ass1.jooq.model.impl.RiderPreference;
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static dst.ass1.jooq.model.public_.Tables.PREFERENCE;
 import static dst.ass1.jooq.model.public_.Tables.RIDER_PREFERENCE;
@@ -23,8 +26,34 @@ public class RiderPreferenceDAO implements IRiderPreferenceDAO {
 
     @Override
     public IRiderPreference findById(Long id) {
-        // FIXME: Implement
-        return null;
+        var dsl = getConnection();
+
+        // First get the keys and values
+        var result = dsl
+                .select()
+                .from(PREFERENCE)
+                .where(PREFERENCE.RIDER_ID.eq(id))
+                .fetch();
+
+        Map<String, String> preferences = new HashMap<>();
+        for (var r : result) {
+            String key = r.get(PREFERENCE.PREF_KEY);
+            String value = r.get(PREFERENCE.PREF_VALUE);
+            preferences.put(key, value);
+        }
+
+        //  Get the rider preference
+        var record = dsl.select().from(RIDER_PREFERENCE)
+                .where(RIDER_PREFERENCE.RIDER_ID.eq(id))
+                .fetchOne();
+
+        RiderPreference riderPreference = new RiderPreference();
+        riderPreference.setRiderId(id);
+        riderPreference.setArea(record.get(RIDER_PREFERENCE.AREA));
+        riderPreference.setVehicleClass(record.get(RIDER_PREFERENCE.VEHICLE_CLASS));
+        riderPreference.setPreferences(preferences);
+
+        return riderPreference;
     }
 
     @Override
@@ -36,7 +65,6 @@ public class RiderPreferenceDAO implements IRiderPreferenceDAO {
     @Override
     public IRiderPreference insert(IRiderPreference model) {
         var dsl = getConnection();
-        // FIXME: This should be a transaction
 
         dsl.transaction((Configuration trx) -> {
             // Insert the Rider

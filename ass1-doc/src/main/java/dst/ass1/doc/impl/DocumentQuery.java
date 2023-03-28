634addab26dbd1651f92d1880214603fcb2aa876
flofriday <flohacksfriday@gmail.com>
2023-03-28 20:58:18 +0200

    Finish 1.4
    MongoDB is actually quite nice, but unfortuanatly the assignment,
    wasn't too clear on what to do and had quite some mistakes.
    
    Still, nice to finally play with MongoDB ^^

--- ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentQuery.java
+++ ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentQuery.java
@@ -1,15 +1,20 @@
 package dst.ass1.doc.impl;
 
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
 import dst.ass1.doc.IDocumentQuery;
 import dst.ass1.jpa.util.Constants;
 import org.bson.Document;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Aggregates.*;
 import static com.mongodb.client.model.Projections.*;
 
 public class DocumentQuery implements IDocumentQuery {
@@ -20,15 +25,37 @@ public class DocumentQuery implements IDocumentQuery {
         this.db = db;
     }
 
+    /*
+    {"_id": {"$oid": "642333865f05715fb5269941"}, "name": "McDonald's", "openHour": 6, "closingHour": 20, "hours": 14}
+    {"_id": {"$oid": "642333865f05715fb5269942"}, "name": "McDonald's", "openHour": 0, "closingHour": 24, "hours": 24}
+    {"_id": {"$oid": "642333865f05715fb5269943"}, "name": "McDonald's", "openHour": 10, "closingHour": 20, "hours": 10}
+    {"_id": {"$oid": "642333865f05715fb5269944"}, "name": "McDonald's", "openHour": 8, "closingHour": 22, "hours": 14}
+    {"_id": {"$oid": "642333865f05715fb526994e"}, "name": "Burger King", "openHour": 10, "closingHour": 24, "hours": 14}
+     */
     @Override
     public List<Document> getAverageOpeningHoursOfRestaurants() {
-        // FIXME: Implement
-        return null;
+        var res = db.getCollection(Constants.COLL_LOCATION_DATA).aggregate(
+                Arrays.asList(
+                        match(eq("category", "Restaurant")),
+                        project(
+                                fields(
+                                        include("name", "closingHour", "openHour"),
+                                        computed("hours",
+                                                new Document("$subtract", Arrays.asList("$closingHour", "$openHour"))
+                                        )
+                                )
+                        ),
+                        group("$name", Accumulators.avg("averageOpeningHours", "$hours"))
+                )
+        ).into(new ArrayList<>());
+
+        res.forEach(doc -> System.out.println(doc.toJson()));
+        return res;
     }
 
     @Override
     public List<Document> findDocumentsByNameWithinPolygon(String name, List<List<Double>> polygon) {
-        return db.getCollection(Constants.COLL_LOCATION_DATA)
+        var res = db.getCollection(Constants.COLL_LOCATION_DATA)
                 .find(
                         and(
                                 geoWithinPolygon("geo", polygon),
@@ -36,6 +63,9 @@ public class DocumentQuery implements IDocumentQuery {
                         ))
                 .projection(fields(include("location_id"), exclude("_id")))
                 .into(new ArrayList<>());
+
+        res.forEach(doc -> System.out.println(doc.toJson()));
+        return res;
     }
 
     @Override

d970513eabe7d50d7198ac3a160eb648f575951e
flofriday <flohacksfriday@gmail.com>
2023-03-28 19:39:00 +0200

    Finish 1.4.2

--- ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentQuery.java
+++ ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentQuery.java
@@ -1,19 +1,20 @@
 package dst.ass1.doc.impl;
 
-import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import dst.ass1.doc.IDocumentQuery;
 import dst.ass1.jpa.util.Constants;
-import org.bson.BsonDocument;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
 
 public class DocumentQuery implements IDocumentQuery {
 
-    private MongoDatabase db;
+    private final MongoDatabase db;
 
     public DocumentQuery(MongoDatabase db) {
         this.db = db;
@@ -27,8 +28,14 @@ public class DocumentQuery implements IDocumentQuery {
 
     @Override
     public List<Document> findDocumentsByNameWithinPolygon(String name, List<List<Double>> polygon) {
-        // FIXME: Implement
-        return null;
+        return db.getCollection(Constants.COLL_LOCATION_DATA)
+                .find(
+                        and(
+                                geoWithinPolygon("geo", polygon),
+                                regex("name", ".*" + Pattern.quote(name) + ".*")
+                        ))
+                .projection(fields(include("location_id"), exclude("_id")))
+                .into(new ArrayList<>());
     }
 
     @Override

bf2ce52bf512738c4651d428fd51c991698bdee1
flofriday <flohacksfriday@gmail.com>
2023-04-20 18:20:13 +0200

    Implement property injection for 2.2.1.1

--- ass2-ioc/src/main/java/dst/ass2/ioc/di/impl/ObjectContainer.java
+++ ass2-ioc/src/main/java/dst/ass2/ioc/di/impl/ObjectContainer.java
@@ -1,22 +1,21 @@
 package dst.ass2.ioc.di.impl;
 
-import dst.ass2.ioc.di.IObjectContainer;
-import dst.ass2.ioc.di.InjectionException;
-import dst.ass2.ioc.di.InvalidDeclarationException;
-import dst.ass2.ioc.di.ObjectCreationException;
+import dst.ass2.ioc.di.*;
 import dst.ass2.ioc.di.annotation.Component;
+import dst.ass2.ioc.di.annotation.Inject;
 import dst.ass2.ioc.di.annotation.Property;
 import dst.ass2.ioc.di.annotation.Scope;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
 public class ObjectContainer implements IObjectContainer {
 
     private Properties properties;
-    private HashMap<Class<?>, Object> singleTons = new HashMap<>();
+    private HashMap<Class<?>, Object> singletons = new HashMap<>();
 
     public ObjectContainer(Properties properties) {
         this.properties = properties;
@@ -35,9 +34,10 @@ public class ObjectContainer implements IObjectContainer {
             throw new InvalidDeclarationException("The class is not a component");
         }
 
-        if (singleTons.containsKey(type))
-            return (T) singleTons.get(type);
+        if (singletons.containsKey(type))
+            return (T) singletons.get(type);
 
+        var properties = (Properties) this.properties.clone();
 
         // Create a new object
         T obj = null;
@@ -51,28 +51,78 @@ public class ObjectContainer implements IObjectContainer {
             throw new ObjectCreationException(e);
         }
 
-        // Fixme: Inject values
-
-        // Set values
-        var fields = type.getDeclaredFields();
+        // Inject values
+        var fields = new java.util.ArrayList<>(List.of(type.getDeclaredFields()));
+        for (var sclass = type.getSuperclass(); sclass != null; sclass = sclass.getSuperclass()) {
+            fields.addAll(List.of(sclass.getDeclaredFields()));
+        }
         for (var field : fields) {
-            var annotation = field.getAnnotation(Property.class);
-            if (null == annotation)
-                continue;
+            var annotation = field.getAnnotation(Inject.class);
+            if (annotation == null) continue;
 
-            /*try {
+            try {
                 field.setAccessible(true);
-                // FIXME: Convert the value to the right type.
-                field.set(obj, properties.getProperty(annotation.value()));
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }*/
+                var injectabel = getObject(annotation.targetType());
+
+                try {
+                    field.set(obj, injectabel);
+                } catch (IllegalAccessException e) {
+                    throw new InjectionException(e);
+                }
+
+            } catch (InjectionException e) {
+                if (!annotation.optional())
+                    throw e;
+            }
+
         }
 
-        // Fixme: Call Initialiye
 
+        // Set values
+        for (var field : fields) {
+            var annotation = field.getAnnotation(Property.class);
+            if (annotation == null)
+                continue;
+
+            var propertyValue = properties.getProperty(annotation.value());
+            if (propertyValue == null)
+                throw new ObjectCreationException(annotation.value() + " is not in the defined in the properties");
+
+            try {
+                field.setAccessible(true);
+                Object value = null;
+                if (field.getType() == String.class) {
+                    value = properties.getProperty(annotation.value());
+
+                } else if (field.getType().isPrimitive()) {
+                    var t = field.getType();
+                    if (t == int.class) {
+                        value = Integer.parseInt(propertyValue);
+                    } else if (t == float.class) {
+                        value = Float.parseFloat(propertyValue);
+                    } else if (t == boolean.class) {
+                        value = Boolean.parseBoolean(propertyValue);
+                    } else {
+                        throw new TypeConversionException("Not supported for type " + t.toString());
+                    }
+
+                } else {
+                    value = field.getType().getDeclaredMethod("valueOf", String.class).invoke(null, propertyValue);
+                }
+                System.out.println("Value: '" + value.toString() + "' " + value.getClass().toString());
+                field.set(obj, value);
+            } catch (IllegalAccessException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new TypeConversionException(e);
+            }
+        }
+
+        // Fixme: Call Init
+
+        // Add to singleton cache
         if (componentAnnotation.scope().equals(Scope.SINGLETON)) {
-            singleTons.put(type, obj);
+            singletons.put(type, obj);
         }
 
         return obj;
